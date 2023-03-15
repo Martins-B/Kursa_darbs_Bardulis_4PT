77a97e5e16133a5682bb98a41be87d26a42033c2
MartinsBardulis <122662287+MartinsBardulis@users.noreply.github.com>
2023-03-15 09:23:21 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,22 +2,21 @@ provider "aws" {
   region = "eu-north-1"
 }
 
-resource "aws_instance" "my_ec2" {
+resource "aws_instance" "my_ec2_deploy" {
   ami           = "ami-02d0b04e8c50472ce"  # Amazon Linux 2 AMI
   instance_type = "t3.micro"
   key_name      = "AWSstockholm"
-  security_groups = [
-    "sg-0cf66e1b712e421c3"  
-  ]
-
-  user_data = <<-EOF
-              #!/bin/bash
-              yum update -y
-              yum install -y docker
-              service docker start
-              EOF
-
+  security_groups = ["sg-0cf66e1b712e421c3"]
+  
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum update -y",
+      "sudo amazon-linux-extras install docker -y",
+      "sudo service docker start",
+      "sudo usermod -a -G docker ec2-user"
+    ]
+  }
   tags = {
-    Name = "my-ec2-instance"
+    Name = "my-ec2-instance_deploy"
   }
 }
