e6816d69f7c64ac2f5a8e132c99688f0f3ea0ce2
MartinsBardulis <122662287+MartinsBardulis@users.noreply.github.com>
2023-03-15 09:39:27 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "my_ec2_deploy" {
   ami           = "ami-02d0b04e8c50472ce"  # Amazon Linux 2 AMI
   instance_type = "t3.micro"
   key_name      = "AWSstockholm"
-  security_groups = ["sg-0cf66e1b712e421c3"]
+  vpc_security_group_ids = ["sg-0cf66e1b712e421c3"]
   
   provisioner "remote-exec" {
     inline = [
